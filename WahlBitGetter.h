bebc6778794763f24f3b3e5102b320c09f9bd6fc
Christopher Wahl <cmwahl@hotmail.com>
2022-01-08 10:54:53 -0500

    Add project files.

---
+++ WahlBitGetter.h
@@ -0,0 +1,32 @@
+﻿// WahlBitGetter.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+class WahlBitGetter {
+public:
+	WahlBitGetter(void* _data, unsigned int _N);
+
+	char get();
+
+	bool setByteLoc(unsigned int loc);
+	bool setBitLoc(unsigned char loc);
+
+	unsigned int getByteLoc();
+	unsigned char getBitLoc();
+
+	bool EOB(); // End of Bits
+	void resetLoc();
+
+	void newData(void* _data, unsigned int _N);
+
+private:
+	unsigned char* data = nullptr;
+	unsigned int N = 0;
+	unsigned int byteLoc = 0;
+	unsigned char bitLoc = 0;
+	unsigned char currentByte = 0;
+
+};
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "WahlBitGetter.h"
+
+using namespace std;
+
+int main()
+{
+
+	unsigned char data[5] = { 170 , 170 , 170 , 170 , 170 };
+	WahlBitGetter getter((void*)data, 5);
+
+	for (int i = 0; i < 5 * 8; ++i) {
+		cout << (short) getter.get();
+	}
+	cout << endl;
+	
+	getter.resetLoc();
+
+	for (int i = 0; i < 5 * 8; ++i) {
+		cout << (short)getter.get();
+	}
+	
+	cout << endl;
+	cout << "EOB: " << getter.EOB() << endl;
+	cout << "get() again: " << (short)getter.get() << endl;
+
+	return 0;
+}
